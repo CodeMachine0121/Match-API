5f2152512b51f543a351984dc760c3d652aab3bd
薛華慶, james.hsueh <james.hsueh@miniasp.com>
2025-07-19 23:32:09 +0800

    feat: implement MatchRepositoryCache for caching match data with IMemoryCache

--- ESportsMatchTracker.API/Program.cs
+++ ESportsMatchTracker.API/Program.cs
@@ -1,6 +1,7 @@
 using ESportsMatchTracker.API.Data;
 using ESportsMatchTracker.API.Data.Entities;
 using ESportsMatchTracker.API.Repositories;
+using ESportsMatchTracker.API.Repositories.Caches;
 using ESportsMatchTracker.API.Repositories.Interfaces;
 using ESportsMatchTracker.API.Services;
 using ESportsMatchTracker.API.Services.Interfaces;
@@ -11,12 +12,14 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllers();
 
+builder.Services.AddMemoryCache();
 builder.Services.AddDbContext<ESportsDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 builder.Services.AddTransient<IMatchService, MatchService>();
-builder.Services.AddTransient<IMatchRepository, MatchRepository>();
 builder.Services.AddTransient<IUnitOfWork, UnitOfWork>();
 
+builder.Services.AddTransient<IMatchRepository, MatchRepository>();
+builder.Services.Decorate<IMatchRepository, MatchRepositoryCache>();
 
 
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
---
+++ ESportsMatchTracker.API/Repositories/Caches/MatchRepositoryCache.cs
@@ -0,0 +1,40 @@
+using System.Text.Json;
+
+using ESportsMatchTracker.API.Models.Ddmains;
+using ESportsMatchTracker.API.Models.Dtos;
+using ESportsMatchTracker.API.Repositories.Interfaces;
+
+using Microsoft.Extensions.Caching.Memory;
+
+namespace ESportsMatchTracker.API.Repositories.Caches;
+
+public class MatchRepositoryCache(IMemoryCache memoryCache, IMatchRepository repository) : IMatchRepository
+{
+    private const string CacheKeyForMatchAll = "match-all";
+
+    public async Task<List<MatchDomain>> GetAllAsync()
+    {
+        if (memoryCache.TryGetValue(CacheKeyForMatchAll, out string? matchesString) && matchesString != null)
+        {
+            return JsonSerializer.Deserialize<List<MatchDomain>>(matchesString)!;
+        }
+        var matches = await repository.GetAllAsync();
+        memoryCache.Set(CacheKeyForMatchAll, JsonSerializer.Serialize(matches), TimeSpan.FromMinutes(10));
+        return matches;
+    }
+    public async Task InsertAsync(InsertMatchDto dto)
+    {
+        memoryCache.Remove(CacheKeyForMatchAll);
+        await repository.InsertAsync(dto);
+    }
+    public async Task UpdateAsync(UpdateMatchDto dto)
+    {
+        memoryCache.Remove(CacheKeyForMatchAll);
+        await repository.UpdateAsync(dto);
+    }
+    public async Task DeleteAsync(int id)
+    {
+        memoryCache.Remove(CacheKeyForMatchAll);
+        await repository.DeleteAsync(id);
+    }
+}
