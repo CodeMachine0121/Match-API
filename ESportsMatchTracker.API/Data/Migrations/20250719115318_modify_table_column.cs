002258bb150da0ecf7c82d0e960a29ca13ba8bb0
薛華慶, james.hsueh <james.hsueh@miniasp.com>
2025-07-19 19:53:51 +0800

    feat: add created_by, created_on, modified_by, and modified_on properties to Match entity for tracking

---
+++ ESportsMatchTracker.API/Data/Migrations/20250719115318_modify_table_column.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ESportsMatchTracker.API.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class modify_table_column : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "created_by",
+                table: "Match",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "created_on",
+                table: "Match",
+                type: "datetime2",
+                nullable: false,
+                defaultValueSql: "GETDATE()");
+
+            migrationBuilder.AddColumn<string>(
+                name: "modified_by",
+                table: "Match",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "modified_on",
+                table: "Match",
+                type: "datetime2",
+                nullable: false,
+                defaultValueSql: "GETDATE()");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "created_by",
+                table: "Match");
+
+            migrationBuilder.DropColumn(
+                name: "created_on",
+                table: "Match");
+
+            migrationBuilder.DropColumn(
+                name: "modified_by",
+                table: "Match");
+
+            migrationBuilder.DropColumn(
+                name: "modified_on",
+                table: "Match");
+        }
+    }
+}
